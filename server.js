105358765ca9b3a2660cd4afe851c3ff7c5d9feb
JoshuaBlount2 <joshuablount2@gmail.com>
2019-03-25 17:51:42 -0500

    added config file, run, and close server functions

--- server.js
+++ server.js
@@ -1,4 +1,67 @@
 const express = require('express');
-const app = express();
-app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
+
+const { router: userRouter } = require('./users');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth')
+
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config')
+
+app.use(morgan('common'));
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+let server;
+
+function runServer() {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(DATABASE_URL, { useMongoClient: true }, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app
+        .listen(PORT, () => {
+          console.log(`Your app is listening on port ${PORT}`);
+          resolve();
+        })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
